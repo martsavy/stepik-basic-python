a7e8d57561a1409cf723ea7d7905a33a98a8d744
s013752 <raphik.small@gmail.com>
2021-07-21 11:31:47 +0200

    Add comments to def's

--- mini-projects/cipher_cezar.py
+++ mini-projects/cipher_cezar.py
@@ -2,7 +2,9 @@ def encrypt(char, key, cardo):
     pass
 def decrypt(char, key, cardo):
     pass
-def get_lang():
+
+def get_lang(): 
+    # ввод языка и проверка введённого значения (должно быть en или ru)
     lang = input("Язык алфавита русский или английский? [ru/en]")
     while True:
         if lang == 'ru' or lang == 'en':
@@ -11,6 +13,7 @@ def get_lang():
             lang = input("Нужно ввести 'ru' или 'en':")
 
 def get_direction():
+    # ввод направления (шифровка/дешифровка) и проверка введённого значения (должно быть e или d)
     direction = input("Введите направление: шифрование или дешифрование; [encrypt=e/decrypt=d]")
     while True:
         if direction == 'e' or direction == 'd':
@@ -18,6 +21,7 @@ def get_direction():
         else:
             direction = input("Нужно ввести 'e' или 'd':")
 def get_key():
+    # ввод сдвига и проверка введённого значения (должно целое число)
     key = input("Введите шаг сдвига (со сдвигом вправо):")
     while True:
         if key.isdigit:
