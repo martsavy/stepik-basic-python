2b9cd94acb3d38dd67178093027114bfddbac383
s013752 <raphik.small@gmail.com>
2021-07-21 13:56:44 +0200

    Working cipher

--- mini-projects/cipher_cezar.py
+++ mini-projects/cipher_cezar.py
@@ -1,7 +1,15 @@
-def encrypt(char, key, cardo):
-    pass
-def decrypt(char, key, cardo):
-    pass
+eng_lower_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+rus_lower_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+rus_upper_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+alphabets = eng_lower_alphabet + eng_upper_alphabet + rus_lower_alphabet + rus_upper_alphabet
+
+def encrypt(char, key, cardo, alphabet):
+    # зашифровка символа-аргумента. Возврат зашифрованного символа
+    return alphabet[((alphabet.find(char) + key) % cardo)]
+def decrypt(char, key, cardo, alphabet):
+    # дешифровка символа-аргумента. Возврат дешифрованного символа
+    return alphabet[((alphabet.find(char) - key) % cardo)]
 
 def get_lang(): 
     # ввод языка и проверка введённого значения (должно быть en или ru)
@@ -20,6 +28,7 @@ def get_direction():
             return direction
         else:
             direction = input("Нужно ввести 'e' или 'd':")
+
 def get_key():
     # ввод сдвига и проверка введённого значения (должно целое число)
     key = input("Введите шаг сдвига (со сдвигом вправо):")
@@ -28,7 +37,43 @@ def get_key():
             return int(key)
         else:
             key = input("Нужно ввести целое число:")
+
+def process(s, direction, lang, key):
+    # установка мощности алфавита
+    if lang == 'ru':
+        cardo = 32
+    else:
+        cardo = 26
+    res = ''
+    for c in s:
+        if direction == 'e':
+            if c in rus_lower_alphabet:
+                res += encrypt(c, key, cardo, rus_lower_alphabet)
+            elif c in rus_upper_alphabet:
+                res += encrypt(c, key, cardo, rus_upper_alphabet)
+            elif c in eng_lower_alphabet:
+                res += encrypt(c, key, cardo, rus_lower_alphabet)
+            elif c in eng_upper_alphabet:
+                res += encrypt(c, key, cardo, eng_upper_alphabet)
+            else:
+                res += c
+        if direction == 'd':
+            if c in rus_lower_alphabet:
+                res += decrypt(c, key, cardo, rus_lower_alphabet)
+            elif c in rus_upper_alphabet:
+                res += decrypt(c, key, cardo, rus_upper_alphabet)
+            elif c in eng_lower_alphabet:
+                res += decrypt(c, key, cardo, rus_lower_alphabet)
+            elif c in eng_upper_alphabet:
+                res += decrypt(c, key, cardo, eng_upper_alphabet)
+            else:
+                res += c
+    return res
+
 # main
 lang = get_lang()
 direction = get_direction()
 key = get_key()
+s = input('Введите текст:')
+
+print(process(s, direction, lang, key))
